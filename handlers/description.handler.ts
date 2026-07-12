a906ea8d9fc8003c9eab43dfb9439b2399bd7e85
nyeinminhtet <nyeinmg904@gmail.com>
2026-07-12 17:30:16 +0630

    feat: add balance when transaction is success to show in message

--- handlers/description.handler.ts
+++ handlers/description.handler.ts
@@ -12,6 +12,7 @@ import { createTransaction } from "@/services/transaction.service";
 import { undoKeyboard } from "@/utils/keyboard";
 
 import { formatCurrency } from "@/utils/formatCurrency";
+import { getBalance } from "@/services/balance.service";
 
 export async function handleDescription(update: TelegramUpdate, user: User) {
   const chatId = getChatId(update);
@@ -48,6 +49,8 @@ export async function handleDescription(update: TelegramUpdate, user: User) {
 
   await clearSession(user.id);
 
+  const currentBalance = await getBalance(user.id);
+
   const typeText = transaction.type === "INCOME" ? "ဝင်ငွေ" : "ထွက်ငွေ";
 
   const descriptionText = transaction.description
@@ -63,6 +66,8 @@ export async function handleDescription(update: TelegramUpdate, user: User) {
       `📂 ကဏ္ဍ - ${transaction.category}`,
       `💰 ပမာဏ - ${formatCurrency(transaction.amount)}`,
       `📝 မှတ်ချက် - ${descriptionText}`,
+      "",
+      `💵 လက်ကျန်ငွေ - ${formatCurrency(currentBalance)}`, // ✨ ဒီမှာ တွဲပြလိုက်တာ ကွက်တိပဲ!
     ].join("\n"),
     {
       reply_markup: undoKeyboard(transaction.id),
